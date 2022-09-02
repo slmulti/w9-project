822bc43532f50d39dc5d07ef3bf1781486e4d64f
Ingrit McKiernan <david.mckiernan@barclays.com>
2022-09-02 11:36:46 +0100

    header changes

--- client/src/Header.js
+++ client/src/Header.js
@@ -1,9 +1,20 @@
+import { formHelperTextClasses } from '@mui/material';
 import { useState, useEffect } from 'react'
+
+const headStyle = {
+    textAlign: "center",
+    color: "black",
+    fontFamily: "Serif",
+    fontSize: 40,   
+    backgroundColor: "white",
+    padding: 10
+}
+
 
 function Header() {
     return(
         <div>
-            <h1 className='header' >SDJ Inventory</h1>
+            <h1 className='header' style={headStyle} >SDJ Inventory</h1>
             <button>Add Item</button>
             <button>Remove Item</button>
         </div>
