032217093b09d308f76b06f0ec0a53e16e33d2d0
James Tierney <james.tierney384@gmail.com>
2022-09-02 10:55:12 +0100

    display single item

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
 import './App.css';
 import { useState, useEffect } from 'react'
 import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions, Grid } from '@mui/material';
+import Item from './Item'
 
 function App() {
 
   const [cards, setCards] = useState([])
-  const [isClicked, setIsClicked] = useState(false)
+  const [current, setCurrent] = useState()
 
   useEffect(() => {
     async function fetchItems () {
@@ -22,44 +18,8 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
-      <div className='card'> {
-        cards.map((card) => {
-          return <>
-          <Grid  container rowSpacing={1} columnSpacing={{xs: 1, sm: 2, md:3, gap: 2}}>
-            <Grid item xs={6}>
-            <Card sx={{ maxWidth: 345, m: 2 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={card.image}
-          alt="green iguana"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div" key={card.id}>
-          { card.title }
-          </Typography>
-          <Typography variant="body2" color="grey" key={card.id}>
-          £{ card.price }
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-      </CardActions>
-      <Button size="small" color="primary" onClick={() => setIsClicked(!isClicked)}>
-      {isClicked ? "Close Description" : "Show Description"}
-      </Button>
-        {isClicked && <Typography variant="body2" color="grey" key={card.id}>
-        { card.description }
-        </Typography>
-        }
-    </Card>
-    </Grid>
-    </Grid>
-          </>
-        })}
-      </div>
+    <div>
+      <Item cards={cards} setCurrent={setCurrent}/>
     </div>
   );
 }
